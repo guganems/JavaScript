b9f3502c05139c9914a7d78e3a348afd449f1361
guganems <g_nemsveridze@cu.edu.ge>
2019-09-05 15:04:59 +0400

    adas

--- Array/Array.concat.js
+++ Array/Array.concat.js
@@ -1,8 +1,13 @@
 Array.prototype.concat = function(...newArr) {
     let old = [...this];
     for (let arr of newArr) {
-        for (let val of arr) {
-            old.push(val);
+        if (Array.isArray(arr)) {
+            for (let val of arr) {
+                old.push(val);
+            }
+        }
+        else {
+            old.push(arr);
         }
     }
     return old;
@@ -10,7 +15,10 @@ Array.prototype.concat = function(...newArr) {
 
 let arr = ['a', 'b', 'c'];
 let arr2 = [1, 2, 3];
+let num1 = [[1]];
+let num2 = [2, [3]];
 let num3 = [7, 8, 9];
+num1[0].push(4);
 
-let newArr = arr.concat(arr2);
+let newArr = num1.concat(num2);
 console.log(newArr);
