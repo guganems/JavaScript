64f1087c8a0e8f05dbc76d6d1fa057a2bfdb56e1
guganems <g_nemsveridze@cu.edu.ge>
2019-09-06 17:31:28 +0400

    dasd

--- Array/array.copyWithin.js
+++ Array/array.copyWithin.js
@@ -1,7 +1,8 @@
 Array.prototype.copyWithin = function (target, start, end) {
+    console.log(this);
     if (end === undefined) end = this.length;
-    else if (end < 0) end  = this.length - end;
-    if (start < 0) start = this.length - start;
+    else if (end < 0) end  = this.length + end;
+    if (start < 0) start = this.length + start;
     let newSeq = [];
     let old = [...this];
     if (target < 0) {
@@ -19,12 +20,17 @@ Array.prototype.copyWithin = function (target, start, end) {
         }
         // TODO: THIS
         else {
-            for (let i = start; i < end; i++) {
+            target = this.length + target;
+            // console.log(end);
+            for (let i = start; i < end-1; i++) {
                 newSeq.push(old[i]);
             }
-            console.log(newSeq);
-            for (let i = 0; i < newSeq.length; i++) {
-                old[i] = newSeq[i];
+            // console.log(newSeq);
+            console.log(target+newSeq.length);
+            let index = 0;
+            for (let i = target; i < target + newSeq.length; i++) {
+                old[i] = newSeq[index];
+                index++;
             }
         }
     }
@@ -40,5 +46,6 @@ Array.prototype.copyWithin = function (target, start, end) {
     return old;
 }
 
-let test = [1, 2, 3, 4, 5].copyWithin(-2, -3, -1);
+// let test = [1, 2, 3, 4, 5].copyWithin(-2, -3, -1);
+let test = [].copyWithin.call({length: 5, 3: 1}, 0, 3);
 console.log(test);
