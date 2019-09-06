f9e8906670baea6e45aff0242b5debb3b471117a
guganems <g_nemsveridze@cu.edu.ge>
2019-09-06 15:02:48 +0400

    Working on negative numbers

--- Array/array.copyWithin.js
+++ Array/array.copyWithin.js
@@ -1,26 +1,44 @@
 Array.prototype.copyWithin = function (target, start, end) {
-    if (end === undefined) end = this.length-1;
+    if (end === undefined) end = this.length;
+    else if (end < 0) end  = this.length - end;
+    if (start < 0) start = this.length - start;
     let newSeq = [];
     let old = [...this];
     if (target < 0) {
-        target = target * -1;
-        for (let i = 0; i < target; i++) {
-            newSeq.push(this[i]);
+        if (start === undefined) {
+            target = target * -1;
+            for (let i = 0; i < target; i++) {
+                newSeq.push(this[i]);
+            }
+            // console.log(newSeq);
+            let newSeqLength = newSeq.length-1;
+            for (let i = this.length-1; i >= this.length-newSeq.length; i--) {
+                old[i] = newSeq[newSeqLength];
+                newSeqLength--;
+            }
         }
-        // console.log(newSeq);
-        let newSeqLength = newSeq.length-1;
-        for (let i = this.length-1; i >= this.length-newSeq.length; i--) {
-            old[i] = newSeq[newSeqLength];
-            newSeqLength--;
+        // TODO: THIS
+        else {
+            for (let i = start; i < end; i++) {
+                newSeq.push(old[i]);
+            }
+            console.log(newSeq);
+            for (let i = 0; i < newSeq.length; i++) {
+                old[i] = newSeq[i];
+            }
         }
     }
     else {
-        for (let i = start+1; i <= end; i++) {
+        for (let i = start; i < end; i++) {
             newSeq.push(old[i]);
+        }
+        // console.log(newSeq);
+        for (let i = 0; i < newSeq.length; i++) {
+            old[i] = newSeq[i];
         }
     }
     return old;
 }
 
-let test = [1, 2, 3, 4, 5].copyWithin(-2);
+let test = [1, 2, 3, 4, 5].copyWithin(-2, -3, -1);
 console.log(test);
